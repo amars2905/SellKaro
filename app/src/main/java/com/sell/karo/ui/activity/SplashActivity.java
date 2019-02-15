0010bb115ff7b48a76f4278f9975da041ce1df92
Pushpendra Bhati <pushpendra.infobite@gmail.com>
2019-02-15 21:59:40 +0530

    new

---
+++ app/src/main/java/com/sell/karo/ui/activity/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.sell.karo.ui.activity;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import com.sell.karo.R;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getKeyHash();
+    }
+
+    private void getKeyHash() {
+
+        PackageInfo info;
+        try {
+            info = getPackageManager().getPackageInfo("com.androidlift.facebookdemo", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                //String something = new String(Base64.encodeBytes(md.digest()));
+                Log.e("hash key", something);
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e("name not found", e1.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("no such an algorithm", e.toString());
+        } catch (Exception e) {
+            Log.e("exception", e.toString());
+        }
+    }
+}
